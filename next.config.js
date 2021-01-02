f3caf0af6fb832111864b2b10731f40942e13fed
Zander Bolgar <lezed1@users.noreply.github.com>
2021-01-02 02:31:35 +0000

    disable next-on-netlify

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   // Target must be serverless
-  target: 'serverless',
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
-
-    // Important: return the modified config
-    return config;
-  },
+  // target: 'serverless',
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   // Note: we provide webpack above so you should not `require` it
+  //   // Perform customizations to webpack config
+  //   config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+  //   // Important: return the modified config
+  //   return config;
+  // },
 };
